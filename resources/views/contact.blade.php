10d3dccc544c472df970a62e0a4b0942d632220e
Bahtiar Rifai <bahtiarderifai@gmail.com>
2023-02-20 17:40:31 +0700

    pwl praktikum 3

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Category</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <form action="" method="post">
+            <label for="nama">Nama</label>
+            <input type="text" name="nama" id="nama">
+            <input type="submit" name="submit">
+        </form>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
@@ -41,6 +42,55 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/about', [PageController::class, 'about']);
 // Route::get('/articles/{id?}', [PageController::class, 'articles']);
 
-Route::resource('/', HomeController::class);
-Route::resource('/about', AboutController::class);
-Route::resource('/articles/{id?}', ArticleController::class);
+// Route::resource('/', HomeController::class);
+// Route::resource('/about', AboutController::class);
+// Route::resource('/articles/{id?}', ArticleController::class);
+
+//praktikum 3
+
+Route::prefix('category')->group(function () {
+    Route::get('/', function () {
+        return view('category');
+    });
+    Route::get('/marbel-edu-games', function () {
+        return "ini adalah halaman category marbel edu games";
+    });
+    Route::get('/marbel-and-friend-kids-games', function () {
+        return "ini adalah halaman category marbel and friend kids games";
+    });
+    Route::get('/riry-story-books', function () {
+        return "ini adalah halaman category riry story books";
+    });
+    Route::get('/kolak-kids-songs', function () {
+        return "ini adalah halaman category kolak kids";
+    });
+});
+
+Route::get('/news/{title}', function ($title) {
+    return "Berita tentang $title";
+});
+
+Route::prefix('program')->group(function () {
+    Route::get('/', function () {
+        return view('program');
+    });
+    Route::get('/karir', function () {
+        return "ini adalah halaman program karir";
+    });
+    Route::get('/magang', function () {
+        return "ini adalah halaman program magang";
+    });
+    Route::get('/kunjungan-industri', function () {
+        return "ini adalah halaman program kunjungan industri";
+    });
+});
+
+Route::get('/about-us', function () {
+    return "Ini adalah halaman about us";
+});
+
+Route::resource('/contact-us', ContactController::class);
+
+Route::get('/', function () {
+    return view('home');
+});
